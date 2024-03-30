e991b6d205e2fc3d079d745a426628cb79140070
monu564100 <144309004+monu564100@users.noreply.github.com>
2024-03-30 12:35:19 +0530

    i

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "Website Title",
+    introTitle: 'Jigyasa..',
+    introDesc: `
+
+    Sometimes, with all the computer stuff, I feel a bit lost, but whenever I think of you, everything just falls into place. You're like the solution to all my coding problems. And behind all the tech stuff, there's just a whole lot of love I have for you.
+    
+    Always yours.`,
+    btnIntro: 'Kutta 😁 ',
+    title: 'Did you fall in love with me at first sight? 🥰',
+    desc: 'Did you get drunk from that smile as soon as you saw it?',
+    btnYes: 'Yes, I love you <33',
+    btnNo: 'Nope :3',
+    question: 'With over 7 billion people in the world, why do you love me <3',
+    btnReply: 'Send to you <3',
+    reply: 'Love is love, And I LOVE YOU <33333333',
+    mess: 'I know 🥰. Love you so much 😘😘',
+    messDesc: 'I Love you 🫂🥺',
+    btnAccept: 'Okiee  <3',
+    messLink: 'https://wa.me/+918789601387?text=iloveyou%20so%20much%20❤' 
+}
